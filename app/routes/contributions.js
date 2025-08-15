a9533fba8f605b64c4a97edc1a4fcd7e95ded50b
Rob Larkin <olorcain@github.com>
2025-08-15 17:49:27 -0400

    testing fix after dismissal

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -29,9 +29,9 @@ function ContributionsHandler(db) {
 
         /*jslint evil: true */
         // Insecure use of eval() to parse inputs
-        const preTax = eval(req.body.preTax);
-        const afterTax = eval(req.body.afterTax);
-        const roth = eval(req.body.roth);
+        const preTax = eval(req.body.preTax); // no change
+        const afterTax = eval(req.body.afterTax); // will fix
+        const roth = eval(req.body.roth); // dismiss as won't fix... but I do!
 
         /*
         //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
